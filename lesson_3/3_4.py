f7b1806e153f3f751f2b4e00d29833fa223f8269
Steaddy <9265214314@mai.ru>
2022-03-25 18:49:25 +0300

    Correction 4 4 task

--- lesson_3/3_4.py
+++ lesson_3/3_4.py
@@ -4,11 +4,13 @@ def my_func(a, b):
 
 def my_func_2(a, b):
     res = a
-    for i in range(b - 1):
+    for i in range(-b - 1):
         res = res * a
-    return res
+    return 1 / res
 
-print(my_func(0.25,-8))
-print(my_func_2(0.25,-8))
 
-# Только если степень целое положительное число
+print(my_func(0.24, -8))
+print(my_func_2(0.24, -8))
+
+# Только если степень целое отрицательное число
+
