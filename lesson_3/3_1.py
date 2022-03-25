ee266f1a7e134a17b123a68bc1d03189abb66be3
Steaddy <9265214314@mai.ru>
2022-03-25 09:31:59 +0300

    Some first task edits

--- lesson_3/3_1.py
+++ lesson_3/3_1.py
@@ -1,7 +1,10 @@
 def divide_nums(n1, n2):
+    if n2 == 0:
+        print("На ноль делить нельзя")
+        return
+
     return round((n1 / n2), 2)
 
 n1 = int(input("Enter number one: "))
 n2 = int(input("Enter number two: "))
-
 print(f"The result is {divide_nums(n1, n2)}")
